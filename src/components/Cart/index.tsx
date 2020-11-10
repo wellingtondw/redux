e9eb3ccf47ce711faf7f58e186c28691ca28c221
wellington <wellingtonslpbi@hotmail.com>
2020-11-10 09:03:29 -0300

    handle state types with redux

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { IState } from '../../store'
+import { ICartItem } from '../../store/modules/cart/types'
 
 const Cart:React.FC = () => {
-  const state = useSelector(state => state)
-
-  console.log(state)
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items)
 
   return (
     <table>
@@ -17,7 +17,14 @@ const Cart:React.FC = () => {
         </tr>
       </thead>
       <tbody>
-
+        {cart.map(item =>(
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity)}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   )
